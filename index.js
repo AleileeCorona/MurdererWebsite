2daa3253efa40190b3ac809972d73466161317d3
AleileeCorona <66973205+AleileeCorona@users.noreply.github.com>
2020-08-14 16:34:48 -0700

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,125 @@
-(function () {
+'use strict';
+
+const apiKey = 'AIzaSyDJsiWIrL8I583xvsI2OYHSNr13TQza-Go'; 
+const searchURL = 'https://www.googleapis.com/youtube/v3/search';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the items array
+  for (let i = 0; i < responseJson.items.length; i++){
+    // for each video object in the items 
+    //array, add a list item to the results 
+    //list with the video title, description,
+    //and thumbnail
+    $('#results-list').append(
+      `<div class="row">
+                <li><h3>${responseJson.items[i].snippet.title}</h3>
+                <p>${responseJson.items[i].snippet.description}</p>
+                <iframe id="ytplayer" type="text/html" width="200" height="200"
+                src="https://www.youtube.com/embed/${responseJson.items[i].id.videoId}?autoplay=0"
+                frameborder="1"></iframe>
+                </li>
+                </div>`
+/* <img src='${responseJson.items[i].snippet.thumbnails.default.url}' class="img-thumbnail">*/
+         //   https://www.youtube.com/watch?v=${responseJson.items[item].id.videoId}
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
+};
+
+function getYouTubeVideos(query, maxResults=5) {
+  const params = {
+   part: 'snippet', 
+   maxResults,
+// id: 'string',
+   key: apiKey,
+   q: query,
+  // items: 'list'
+  };
+  const queryString = formatQueryParams(params)
+  const url = `${searchURL}?${queryString}`;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+        console.log(response)
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#err').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function getWikiInfo(query, maxResults=5){
+const wikiParams = {
+    action: 'query',
+    list: 'search',
+    format: 'json',
+    rvsection: "0"
+  };
+  
+
+// call to Wikipedia API
+function searchWiki(searchWord) {
+    wikiParams.titles = searchWord;
+    url = 'https://en.wikipedia.org/w/api.php?action=query&list=search$format=json&origin=*&srsearch='+s);
+
+
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#murderer').val();
+    const maxResults = $('#js-max-results').val();
+    getYouTubeVideos(searchTerm, maxResults);
+  });
+}
+
+
+$(watchForm);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*(function () {
 
     'use strict';
     
@@ -71,9 +192,4 @@
         }
     
     })()
-
-
-
-
-
-
+*/
